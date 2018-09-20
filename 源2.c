4d7a24ead5852059069b88493a11498a6546feb0
Sirlanri <maoyingyu00@gmail.com>
2018-09-20 11:01:30 +0800

    暂停程序的实例

---
+++ 源2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning (disable:4699)
+int main()
+{
+
+	printf("I need a pause here.\n");
+
+	system("pause");
+
+	printf("And here too.\n");
+
+	system("pause");
+
+	return 0;
+
+}
