d716b6830c8e08b4bc1385fb5fa3f96223cf8489
Sirlanri <ricosirlanri@gmail.com>
2018-09-22 09:58:17 +0800

    Create 1.c

---
+++ Class/1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{	int sex;
+printf( "hello Internet Of Things \n");
+printf( "我他喵的不会盲打啊啊啊 \n");
+printf("好无聊的编程课啊...");
+ 
+
+	printf("please enter your gender.man 1    feman 2");
+	scanf("%d",&sex);
+	if (sex==1)
+		printf("你真可爱\n");
+	else
+		printf("cool~~~~~~\n");
+printf ("let's make it this fucking damn C艹");
+
+}
