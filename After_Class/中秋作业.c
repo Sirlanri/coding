ecee612cd026d595a6a7ee4bbc92dab356b4ca9a
Sirlanri <ricosirlanri@gmail.com>
2018-09-24 20:14:18 +0800

    Update 中秋作业.c
    这个版本还有BUG，我还不能修复.......

--- After_Class/中秋作业.c
+++ After_Class/中秋作业.c
@@ -1,19 +1,28 @@
 #include<stdio.h>
 #pragma warning (disable:4996)
 int main()
-{ int=A;
-	printf ("回家否？Y/N/n");
-   scanf（"%d",&A）;
- if(A=Y){
-	printf("****************\n");
-	printf("*ÖÐ            *\n");
-	printf("*   Çï         *\n");
-	printf("*       ¿ì     *\n");
-	printf("*           ÀÖ *\n");
-	printf("****************\n");
-     }
-  else
-	 {
-
-return 0;
-}
+{
+	int  A;
+	int Y;
+	int N;
+	printf("回家否？YorN/n");
+	scanf("%d", &A);
+	if (A==Y) {
+		printf("****************\n");
+		printf("*中            *\n");
+		printf("*   秋         *\n");
+		printf("*       快     *\n");
+		printf("*           乐 *\n");
+		printf("****************\n");
+	           }
+	else
+	   {
+		printf("****************\n");
+		printf("*     中       *\n");
+		printf("*     秋       *\n");
+		printf("*     快       *\n");
+		printf("*     乐       *\n");
+		printf("****************\n");
+	    }
+		return 0;
+	}
