9264e01713544072b32be00bb599f1d52a55e7d6
Sirlanri <ricosirlanri@gmail.com>
2018-09-24 10:28:34 +0000

    Update 中秋作业.c

--- After_Class/中秋作业.c
+++ After_Class/中秋作业.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 #pragma warning (disable:4996)
 int main()
-{
+{ int=Y
+  int=N
+	printf ("回家否？Y/N/n");
+   scanf（"%d",&A）;
+ if(A=Y){
 	printf("****************\n");
 	printf("*ÖÐ            *\n");
 	printf("*   Çï         *\n");
 	printf("*       ¿ì     *\n");
 	printf("*           ÀÖ *\n");
 	printf("****************\n");
-	
+     }
+  else
+	 {
+
 return 0;
 }
