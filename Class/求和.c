b47270ed119387e36ba0755b8fc05fffd8f4a248
Sirlanri <ricosirlanri@gmail.com>
2018-09-26 03:28:24 +0000

    Create 求和.c
    前n个数求和

---
+++ Class/求和.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#pragam warning(disable:4996)
+
+int main()
+{
+int i,n;
+double sum=0:
+
+ printf("输入一个数字吧"/n);
+ scanf("%d",&n);
+ 
+ for(i=1;i<n+1,i++)
+  {
+  sum=sum+1.0/i
+ 
+  }
+ printf("%f",sum);
+}
