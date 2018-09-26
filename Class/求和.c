856539a54184f55820720178ec5cf7b3feef0d92
Sirlanri <ricosirlanri@gmail.com>
2018-09-26 03:31:42 +0000

    Update 求和.c

--- Class/求和.c
+++ Class/求和.c
@@ -4,14 +4,14 @@
 int main()
 {
 int i,n;
-double sum=0:
+double sum=0;
 
  printf("输入一个数字吧"/n);
  scanf("%d",&n);
  
  for(i=1;i<n+1,i++)
   {
-  sum=sum+1.0/i
+  sum=sum+1.0/i;
  
   }
  printf("%f",sum);
